ec771f9f140977f94718fc39f32bb3a25e0e3292
Bhaskar Thakur <bhaskar@collobe.com>
2021-09-09 18:48:53 +0530

    Add prod config

--- post_build.sh
+++ post_build.sh
@@ -1,1 +1,10 @@
-aws codepipeline start-pipeline-execution --name SociomataStagingStatic
+string=$CODEBUILD_BUILD_ID;
+delimiter=":"
+declare -a array=($(echo $string | tr "$delimiter" " "))
+project=${array[0]}
+if [ "$project" == "StagingElm" ]
+then
+  aws codepipeline start-pipeline-execution --name SociomataStagingStatic
+else
+  aws codepipeline start-pipeline-execution --name ProdStatic
+fi
