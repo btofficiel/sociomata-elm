740c51504af4599d3ee81bd6aff5d459fcc44ad4
Bhaskar Thakur <bhaskar@collobe.com>
2021-09-09 21:26:46 +0530

    Fix delete old script

--- delete_old.sh
+++ delete_old.sh
@@ -1,2 +1,12 @@
-ELM_KEY=$(aws s3api list-objects-v2  --bucket "sociomata-staging-static" --prefix "js/elm" --query Contents[0].Key)
-aws s3api delete-objects --bucket "sociomata-staging-static" --delete Objects=[{Key=$ELM_KEY}]
+string=$CODEBUILD_BUILD_ID;
+delimiter=":"
+declare -a array=($(echo $string | tr "$delimiter" " "))
+project=${array[0]}
+if [ "$project" == "StagingElm" ]
+then
+  ELM_KEY=$(aws s3api list-objects-v2  --bucket "sociomata-staging-static" --prefix "js/elm" --query Contents[0].Key)
+  aws s3api delete-objects --bucket "sociomata-staging-static" --delete Objects=[{Key=$ELM_KEY}]
+else
+  ELM_KEY=$(aws s3api list-objects-v2  --bucket "sociomata-staging-static" --prefix "js/elm" --query Contents[0].Key)
+  aws s3api delete-objects --bucket "sociomata-staging-static" --delete Objects=[{Key=$ELM_KEY}]
+fi
