7c31a316c0b8e4ad034369659f77041f98dde357
Bhaskar Thakur <bhaskar@collobe.com>
2021-09-10 12:53:48 +0530

    Fix deleting of staging bundle from prod codebuild setup

--- delete_old.sh
+++ delete_old.sh
@@ -4,9 +4,11 @@ declare -a array=($(echo $string | tr "$delimiter" " "))
 project=${array[0]}
 if [ "$project" == "StagingElm" ]
 then
+  echo "Deleting staging static elm bundle"
   ELM_KEY=$(aws s3api list-objects-v2  --bucket "sociomata-staging-static" --prefix "js/elm" --query Contents[0].Key)
   aws s3api delete-objects --bucket "sociomata-staging-static" --delete Objects=[{Key=$ELM_KEY}]
 else
+  echo "Deleting prod static elm bundle"
   ELM_KEY=$(aws s3api list-objects-v2  --bucket "sociomata-prod-static" --prefix "js/elm" --query Contents[0].Key)
   aws s3api delete-objects --bucket "sociomata-prod-static" --delete Objects=[{Key=$ELM_KEY}]
 fi
